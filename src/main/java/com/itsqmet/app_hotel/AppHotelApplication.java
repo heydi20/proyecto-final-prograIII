fcf4058414c8b1b49dca26925f31a05601592030
heydi20 <heydipantoja1@gmail.com>
2025-01-28 10:23:44 -0500

    first commit

---
+++ src/main/java/com/itsqmet/app_hotel/AppHotelApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.app_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsqmet/app_hotel/AppHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.app_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
