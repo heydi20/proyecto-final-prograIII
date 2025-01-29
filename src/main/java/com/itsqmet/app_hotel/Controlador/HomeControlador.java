43ff4990eb513c391c96d92a0abde7066023aa3c
DanyFJ2001 <danyjoel345@gmail.com>
2025-01-29 08:11:04 -0500

    pagina

---
+++ src/main/java/com/itsqmet/app_hotel/Controlador/HomeControlador.java
@@ -0,0 +1,15 @@
+package com.itsqmet.app_hotel.Controlador;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeControlador {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+
+
+}
